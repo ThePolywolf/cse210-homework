6c38fe20d7a5e174f1c7b09696c0803668808b13
ThePolywolf <103151618+ThePolywolf@users.noreply.github.com>
2023-09-29 14:27:32 -0700

    Completed Learning05 Assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Welcome Message
+        WelcomeMessage();
+
+        // Prompt name
+        string userName = PromptForName();
+
+        // Prompt number
+        int favNumber = PrompForNumber();
+
+        // Display results
+        Respond(userName, favNumber);
+    }
+
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the C# Program!");
+    }
+
+    static string PromptForName()
+    {
+        Console.Write("What is your name? ");
+        return Console.ReadLine();
+    }
+
+    static int PrompForNumber()
+    {
+        while (true)
+        {
+            Console.Write("What is your favorite integer? ");
+            string response = Console.ReadLine();
+
+            if (int.TryParse(response, out int number))
+            {
+                return number;
+            }
+
+            Console.WriteLine($"> {response} isn't an integer.");
+        }
+    }
+
+    static void Respond(string userName, int favNumber)
+    {
+        int numberSquared = favNumber * favNumber;
+
+        Console.WriteLine($"Hello {userName}! Your favorite number squared is {numberSquared}.");
     }
 }
