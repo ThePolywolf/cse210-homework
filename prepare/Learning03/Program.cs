d4e42a1030576ac961252f98b98f8cda8b9e0e91
ThePolywolf <103151618+ThePolywolf@users.noreply.github.com>
2023-10-10 13:20:03 -0700

    Finished Prepare/Learning03 Fraction class encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        // rip
+        List<Fraction> fractions = new List<Fraction>();
+
+        fractions.Add(new Fraction());
+        fractions.Add(new Fraction(5));
+        fractions.Add(new Fraction(3, 4));
+        fractions.Add(new Fraction(1, 3));
+
+        foreach (Fraction fraction in fractions){
+            Console.WriteLine(fraction.GetFractionString());
+            Console.WriteLine(fraction.GetDecimal().ToString());
+        }
     }
 }
